c0543b9c01fb71b94daf3a947fb7d09006c63590
dy chen <cctfs@live.ca>
2019-01-23 03:12:07 -0500

    added tp

---
+++ TP/tp1/tp1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int estParfait(int a){
+	int parfait = 0;
+
+	//peut-on conclure qu’il n’y a pas de nombre parfait impair?
+	if(a%2!=0){
+		printf("bye\n");
+		return parfait;
+	}
+	
+
+	
+
+	return parfait; 
+}
+
+int main(int argc, char *argv[]) {
+
+	if(estParfait(3))
+	
+	printf("Hello, world!\n");
+
+        return 0;
+}
