bca4655c525d00f1b508c989f9faa38c1194299c
dy chen <cctfs@live.ca>
2019-01-25 01:20:14 -0500

    trouver algo pour nombre parfait

--- TP/tp1/tp1.c
+++ TP/tp1/tp1.c
@@ -1,25 +1,52 @@
 #include <stdio.h>
 
-int estParfait(int a){
-	int parfait = 0;
+//nb parfait = la somme de toute ses diviseurs except lui-meme
+
+long estParfait(long n){
+
+	long parfait = 0;
+	long somme = 1;
 
 	//peut-on conclure qu’il n’y a pas de nombre parfait impair?
-	if(a%2!=0){
-		printf("bye\n");
+	if(n%2!=0){
+		//printf(“bye\n");
 		return parfait;
 	}
-	
 
+	//on commence a 2 car sert a rien de tester 1, vu qu’on ne peut pas ajouter lui-meme
+	for(long i = 2; i*i<=n; i++){
 	
+		if(n%i==0){
+			if(i*i!=n){
+				somme = somme + i + (n/i);
+			} else {
+				somme = somme + i ;
+			}
+		}
+	}
+	
+	if(somme == n){
+		parfait =1;
+	}
 
 	return parfait; 
 }
 
 int main(int argc, char *argv[]) {
-
-	if(estParfait(3))
 	
-	printf("Hello, world!\n");
+	for(long i = 0; i<= 137438691328; i++){
+
+		if(estParfait(i) && i%2==0){
+			printf("%ld",i);	
+			printf(" nb parfait\n");
+		}
+
+		else {
+			printf("%ld\n",i);
+		}
+
+	}
+		
 
         return 0;
 }
